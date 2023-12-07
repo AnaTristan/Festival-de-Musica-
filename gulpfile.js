5815d7b6f4d075fe298a7d978b7c581670dd07b0
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2023-12-06 23:33:29 -0600

    cambios menores

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,23 @@
 
-const { src, dest} = require("gulp");
+const { src, dest, watch} = require("gulp");
 const sass = require("gulp-sass")(require('sass'));
 
 // compiling sass
 function css(done){
 
-    src("src/scss/app.scss")
+    src("src/scss/**/*.scss")
     .pipe(sass())
     .pipe(dest("build/css"));
 
     done();
 }
 
+// watch 
+function dev(done){
+    watch("src/scss/**/*.scss",css)
+
+    done();
+}
+
 exports.css = css;
+exports.dev=dev;
