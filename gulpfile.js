2b037fde67847ea0bd450fe841245b2388878ffc
Ana Lucia Coronado Tristan <alct0397@gmail.com>
2023-12-27 18:42:15 -0600

    webp img

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,52 @@
 
-const { src, dest, watch} = require("gulp");
+const { src, dest, watch } = require("gulp");
+//parallel
+
 const sass = require("gulp-sass")(require('sass'));
-const plumber=require('gulp-plumber');
+const plumber = require('gulp-plumber');
+
+//Imagenes
+const imagemin = require('gulp-imagemin');
+const cache = require('gulp-cache');
+//const webp = require('gulp-webp');
+
+//function verWebp(done) {
+//const opciones = {
+//quality: 50
+// };
+
+// src('src/img/**/*.{png,jpg}')
+// .pipe(webp(opciones))
+//.pipe(dest("build/img"))
+//done();
+//}
+
+function imagenes(done) {
+
+    const opciones = {
+        optimizationLevel: 3
+    };
+
+    src('src/scss/**/*.scss')
+        .pipe(cache(imagemin(opciones)))
+        .pipe(dest("build/img"))
+    done();
+}
 
 
 // compiling sass
-function css( done ){
+function css(done) {
 
     src('src/scss/**/*.scss')
-    .pipe(plumber())
-    .pipe(sass())
-    .pipe(dest("build/css"));
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(dest("build/css"));
 
     done();
 }
 
 // watch 
-function dev( done ){
+function dev(done) {
     watch('src/scss/**/*.scss', css);
 
     done();
@@ -24,3 +54,6 @@ function dev( done ){
 
 exports.css = css;
 exports.dev = dev;
+exports.imagenes = imagenes;
+//exports.verWebp = verWebp;
+// exports.dev=parallel(imagenes,verWebp,dev);/
